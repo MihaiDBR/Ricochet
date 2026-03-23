03ef44b56346a07f1ecb784990c8a361c7b48eea
MihaiDBR <MihaiDBR@users.noreply.github.com>
2026-03-23 17:24:59 +0200

    Initialized Workspace and added Paddle and Ball classes for game simulation.

--- main.cpp
+++ main.cpp
@@ -1,55 +1,88 @@
 #include <iostream>
 #include <array>
-#include "include/Example.h"
-// This also works if you do not want `include/`, but some editors might not like it
-// #include "Example.h"
+#include <vector>
+#include <cmath>
+
+class Paddle {
+
+private:
+    float x, y, height, width, speed;
+
+public:
+    Paddle(float x, float y, float height, float width, float speed)
+        : x(x),
+          y(y),
+          height(height),
+          width(width),
+          speed(speed) {
+    }
+
+    void moveLeft(float screenLeft) {
+        if (x - speed >= screenLeft) {
+            x -= speed;
+        }
+    }
+
+    void moveRight(float screenRight) {
+        if (x + speed + width <= screenRight) {
+            x += speed;
+        }
+    }
+
+    float getX() const { return x;}
+    float getY() const { return y;}
+    float getHeight() const { return height;}
+    float getWidth() const { return width;}
+
+    friend std::ostream& operator<<(std::ostream& os, const Paddle& paddle) {
+        os << "Paddle " << "[" << "x=" << paddle.x << ", y=" << paddle.y << ", width=" << paddle.width <<
+            ", height=" << paddle.height << ", speed=" << paddle.speed << "]";
+        return os;
+    };
+};
+
+class Ball {
+
+private:
+    float x, y, dx, dy, radius;
+
+public:
+
+    Ball(float x, float y, float dx, float dy, float radius)
+        : x(x),
+          y(y),
+          dx(dx),
+          dy(dy),
+          radius(radius) {
+    }
+
+    void move() {
+        x += dx;
+        y += dy;
+    }
+
+    void bounceX() {
+        dx = -dx;
+    }
+
+    void bounceY() {
+        dy = -dy;
+    }
+
+    float getX() const { return x;}
+    float getY() const { return y;}
+    float getRadius() const { return radius;}
+    float getDy() const { return dy;}
+
+    friend std::ostream& operator<<(std::ostream& os, const Ball& ball) {
+        os << "Ball " << "[" << "x=" << ball.x << ", y=" << ball.y << ", dx=" << ball.dx <<
+            ", dy=" << ball.dy << ", radius=" << ball.radius << "]";
+        return os;
+    }
+
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
-    Example e1;
-    e1.g();
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+
     return 0;
 }
